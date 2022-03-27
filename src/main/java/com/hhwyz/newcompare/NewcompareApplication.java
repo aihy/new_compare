18012e9585d62ea082720a81e91872bc74149d9b
aihy <wzhy1234@qq.com>
2022-03-27 22:20:33 +0800

    init

---
+++ src/main/java/com/hhwyz/newcompare/NewcompareApplication.java
@@ -0,0 +1,13 @@
+package com.hhwyz.newcompare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewcompareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewcompareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hhwyz/newcompare/NewcompareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhwyz.newcompare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewcompareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
